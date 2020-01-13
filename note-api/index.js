04acafa3634988a8f30e799d4628a86431e77bf8
dpgian <dpgian@gmail.com>
2020-01-13 17:06:21 +0000

    mongoose and db connected

--- note-api/index.js
+++ note-api/index.js
@@ -1,7 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+const dburi = 'mongodb+srv://dpgian:notepassword@note-db-5xspy.mongodb.net/test?retryWrites=true&w=majority'
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dburi, { useNewUrlParser: true })
+        console.log('Database connected..')
+    } catch(err) {
+        console.log('Error on database connection: ' +err)
+        process.exit(1)
+    }
+}
+
+connectDB()
 
 const app = express()
 const PORT = 4000
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+})
 
 app.get('/', (req, res) => {
     res.json({
@@ -9,6 +28,4 @@ app.get('/', (req, res) => {
     })
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
-})
+//password: notepassword
