846fbd25964592fb9e1abc09476372c27b89f6b1
dpgian <dpgian@gmail.com>
2020-01-14 00:41:25 +0000

    CRA with react router

---
+++ note-ui/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import AllNotes from './AllNotes'
+import EditNote from './EditNote'
+import NewNote from './NewNote'
+
+class App extends React.Component{
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav className='navbar App-header' role='navigator' aria-label='main navigation'>
+            <div className='navbar-brand'>
+              <Link to='/' className='navbar-item'>
+                Notes App
+              </Link>
+            </div>
+
+            <div className='navbar-end'>
+              <Link to='/' className='navbar-item'>
+                All Notes
+              </Link>
+
+              <Link to='/newnote' className='navbar-item'>
+                New Note
+              </Link>
+            </div>
+          </nav>
+
+          <Route exact path='/' component={AllNotes} />
+          <Route path='/newnote' component={NewNote} />
+          <Route path='/note/:id' component={EditNote} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App;
