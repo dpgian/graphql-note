fc053ea2a7a8548c30fc18148764aaa6e2fbac75
dpgian <dpgian@gmail.com>
2020-01-14 01:06:49 +0000

    Views created

--- note-ui/src/App.js
+++ note-ui/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import AllNotes from './AllNotes'
 import EditNote from './EditNote'
