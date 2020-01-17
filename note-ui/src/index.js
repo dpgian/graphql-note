cb457c6b6938ee23042fd7546d5fb57f80c1acf1
dpgian <dpgian@gmail.com>
2020-01-17 16:26:24 +0000

    Hooked ApolloClient

--- note-ui/src/index.js
+++ note-ui/src/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const httpLink = createHttpLink({ uri: 'https://localhost:4000/graphql' })
+
+const link = ApolloLink.from([httpLink])
+
+const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, 
+    document.getElementById('root')
+);
